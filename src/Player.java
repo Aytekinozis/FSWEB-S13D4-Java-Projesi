b89bdb4d41a8e6121ee283591caf4048d6b4aa2e
Aytekinozis <aytekinozis93@gmail.com>
2024-01-11 20:14:37 +0300

    Proje tamamlandi

---
+++ src/Player.java
@@ -0,0 +1,40 @@
+public class Player {
+    private String name;
+    private int healthPercentage;
+    private Weapon weapon;
+
+    public enum Weapon {
+        BroadSword,
+        Katana,
+        Spear,
+        Wand
+    }
+    public Player(String name, int healthPercentage, Weapon weapon) {
+        this.name = name;
+        if (healthPercentage > 100) {
+            this.healthPercentage = 100;
+        } else this.healthPercentage = Math.max(healthPercentage, 0);
+        this.weapon = weapon;
+    }
+
+    public int healthRemaining() {
+        return this.healthPercentage;
+    }
+    public void loseHealth(int damage) {
+        if (this.healthPercentage > 0) {
+            this.healthPercentage -= damage;
+            if(this.healthPercentage<0){
+                System.out.println(name + " player has been knocked out of game");
+            }
+
+        } else {
+            System.out.println(name + " player has been knocked out of game");
+        }
+    }
+    public void restoreHealth(int healthPotion) {
+        this.healthPercentage += healthPotion;
+        if (this.healthPercentage > 100) {
+            this.healthPercentage = 100;
+        }
+    }
+}
